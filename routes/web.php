84ad03b0e4337e950e815f6021cbfa44ff6ed526
armincv <armincvaldez@gmail.com>
2024-03-11 14:44:11 +0800

    Added Routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/about', 'AboutController@index')->name('about');
+Route::get('/portfolio', 'PortfolioController@index')->name('portfolio');
+Route::get('/contact', 'ContactController@index')->name('contact');
