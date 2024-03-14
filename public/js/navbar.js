561ece721e14eeeee0eb048e1365fbafc0bd021e
armincv <armincvaldez@gmail.com>
2024-03-14 16:45:58 +0800

    Navbar is sticky

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,5 +1,3 @@
-import './bootstrap';
-
 document.addEventListener('DOMContentLoaded', () => {
     const navbarHeight = document.querySelector('.navbar').offsetHeight;
     const contentContainer = document.querySelector('.content-container');
